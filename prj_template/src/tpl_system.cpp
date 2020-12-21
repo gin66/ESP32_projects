093ebd6998b700b782944f7eb7dd12af89c5387b
Jochen Kiemes <jochen@kiemes.de>
2020-12-21 11:04:49 +0100

    only output stack info for running tasks

--- prj_template/src/tpl_system.cpp
+++ prj_template/src/tpl_system.cpp
@@ -16,22 +16,20 @@ struct tpl_config_s tpl_config = {.bootCount = 0,
                                   .ota_ongoing = false,
 								  .stack_info = stack_info};
 
+#define ADD_STACK_INFO(info_fmt,task) {\
+	if(task != NULL) {\
+		p += sprintf(p, info_fmt, uxTaskGetStackHighWaterMark(task));\
+	}\
+}
 void tpl_update_stack_info() {
-	sprintf(stack_info,
-    "Stackfree: loop=%d" \
-    " net_watchdog=%d" \
-    " websocket=%d" \
-    " command=%d" \
-    " wifi=%d" \
-    " http=%d" \
-    " bot=%d",
-    uxTaskGetStackHighWaterMark(NULL),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_net_watchdog),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_websocket),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_command),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_wifi_manager),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_webserver),
-    uxTaskGetStackHighWaterMark(tpl_tasks.task_telegram));
+	char *p = stack_info;
+	p += sprintf(p, "Stackfree: this=%d", uxTaskGetStackHighWaterMark(NULL));
+    ADD_STACK_INFO(" net_watchdog=%d",tpl_tasks.task_net_watchdog);
+    ADD_STACK_INFO(" websocket=%d",tpl_tasks.task_websocket);
+    ADD_STACK_INFO(" command=%d",tpl_tasks.task_command);
+    ADD_STACK_INFO(" wifi=%d",tpl_tasks.task_wifi_manager);
+    ADD_STACK_INFO(" http=%d",tpl_tasks.task_webserver);
+    ADD_STACK_INFO(" bot=%d",tpl_tasks.task_telegram);
 }
 
 static RTC_DATA_ATTR uint32_t bootCount = 0;

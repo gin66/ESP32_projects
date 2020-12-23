6977887060f6190bbc3541668c110a09a78c3d54
Jochen Kiemes <jochen@kiemes.de>
2020-12-23 17:17:38 +0100

    add prj_00_starter

---
+++ prj_00_starter/src/main.cpp
@@ -0,0 +1,38 @@
+#include "template.h"
+
+using namespace std;
+
+//---------------------------------------------------
+void setup() {
+  tpl_system_setup(0);  // no deep sleep
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+
+  // Wait OTA
+  tpl_wifi_setup(true, true, (gpio_num_t)tpl_ledPin);
+  tpl_webserver_setup();
+  tpl_websocket_setup(NULL);
+  tpl_net_watchdog_setup();
+  tpl_command_setup(NULL);
+
+#ifdef IS_ESP32CAM
+  uint8_t fail_cnt = 0;
+#ifdef BOTtoken
+  tpl_camera_setup(&fail_cnt, FRAMESIZE_QVGA);
+#else
+  tpl_camera_setup(&fail_cnt, FRAMESIZE_VGA);
+#endif
+#endif
+
+#ifdef BOTtoken
+  tpl_telegram_setup(BOTtoken, CHAT_ID);
+#endif
+
+  Serial.println("Setup done.");
+}
+
+void loop() {
+  const TickType_t xDelay = 10 / portTICK_PERIOD_MS;
+  vTaskDelay(xDelay);
+}

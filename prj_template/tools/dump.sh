685792ff94ba4550a63ab9823fd63e6b9cada7e3
Jochen Kiemes <jochen@kiemes.de>
2020-12-25 12:52:59 +0100

    new tool: dumpsh

---
+++ prj_template/tools/dump.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+TARGET=$1
+if [ "$TARGET" == "" ]
+then
+	TARGET=`gawk 'BEGIN{FS=" *= *"} /hostname/ && /esp32_/{print $2".local"}' platformio.ini`
+fi
+
+echo $TARGET
+
+if [ "$TARGET" == "" ]
+then
+	echo Cannot get target
+	exit
+fi
+
+if [ ! -f ~/.cargo/bin/websocat ]
+then
+	cargo install websocat
+fi
+
+# wait esp coming online
+while true
+do
+	echo -n .
+	ping -c 1 $TARGET
+	if [ $? == 0 ]
+	then
+		break
+	fi
+	sleep 1
+done
+
+while true
+do
+	echo -n .
+	nc -z $TARGET 80
+	echo $?
+	if [ $? == 0 ]
+	then
+		break
+	fi
+	sleep 1
+done
+
+wget http://$TARGET/dump

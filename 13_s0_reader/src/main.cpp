8d9206389808f36be687585ef9a2779447e30eb1
Jochen Kiemes <jochen@kiemes.de>
2023-07-23 23:08:46 +0200

    derive current power from pulse delta

--- 13_s0_reader/src/main.cpp
+++ 13_s0_reader/src/main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 // can be used as parameter to tpl_command_setup
 // void execute(enum Command command) {}
 
-uint32_t wh_last_second = 0;
-uint32_t wh_last_minute = 0;
-uint32_t wh_last_hour = 0;
+uint32_t w_last_second = 0;
+uint32_t w_last_minute = 0;
+uint32_t w_last_hour = 0;
+uint32_t w_current = 0;
 uint32_t pulse = 0;
 uint32_t pulseAtSecond[60] = {
 	0,0,0,0,0,0,0,0,0,0,
@@ -37,9 +38,10 @@ void publish_func(DynamicJsonDocument * json) {
   (*json)["aport"] = analogRead(34);
   (*json)["dport"] = digitalRead(34);
   (*json)["pulse"] = pulse;
-  (*json)["Wh_s"] = wh_last_second;
-  (*json)["Wh_m"] = wh_last_minute;
-  (*json)["Wh_h"] = wh_last_hour;
+  (*json)["W_last_s"] = w_last_second;
+  (*json)["W_last_m"] = w_last_minute;
+  (*json)["W_last_h"] = w_last_hour;
+  (*json)["W"] = w_current;
 }
 
 //---------------------------------------------------
@@ -74,20 +76,36 @@ void setup() {
 }
 
 time_t last = 0;
+uint32_t last_pulse_ms = 0;
+
 void loop() {
   taskYIELD();
+  // the S0 pulses are rather slow, so we just do polling
   if (analogRead(34) < 4000) {
 	time_t now = time(nullptr);
     pulse++;
+	uint32_t ms = millis();
+	uint32_t dt = ms - last_pulse_ms;
+	last_pulse_ms = ms;
+	if ((dt < 10000) && (dt != 0)) {
+		// 10 Imp/Wh => 1 Imp equals 0.1Wh
+		// P = 0.1Wh * 1/(dt * 1ms)
+		// 1 ms = 1/(3600*1000) h
+		// P = 0.1W * 3600*1000/dt
+		//   = 360000/dt
+		w_current = 360000/dt;
+	}
 
 	uint8_t now_s = now % 60;
 	uint8_t now_m = (now / 60) % 60;
 	uint8_t now_h = (now / 3600) % 24;
 
     if (last != now) { // new second
-	  wh_last_second = pulseAtSecond[(now_s+59)%60];
-	  wh_last_minute = pulseAtMinute[(now_m+59)%60];
-	  wh_last_hour = pulseAtHour[(now_m+23)%24];
+	  // 10000 Imp/kWh = 10 Imp/Wh
+	  // pulse / (10 Imp/Wh) yields Wh
+	  w_last_second = pulseAtSecond[(now_s+59)%60] * 360; /// Wh * 3600 (sec/h) * 1/sec / 10
+	  w_last_minute = pulseAtMinute[(now_m+59)%60] * 6; // Wh * 60 (min/h) * 1/min / 10
+	  w_last_hour = pulseAtHour[(now_m+23)%24] / 10;
 
 	  pulseAtSecond[now_s] = 0;
 	  if (now_s == 0) {

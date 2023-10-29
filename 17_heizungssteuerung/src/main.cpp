f96af48e9d015d328c8db90f7526e968c5d1a0d4
Jochen Kiemes <jochen@kiemes.de>
2023-10-29 12:30:00 +0100

    describe 17 hardware

--- 17_heizungssteuerung/src/main.cpp
+++ 17_heizungssteuerung/src/main.cpp
@@ -1,7 +1,51 @@
-#include "base64.h"
+#include <Arduino.h>
+#include <base64.h>
 #include "template.h"
 
 using namespace std;
+
+// SuperMINI Esp32c3: https://www.nologo.tech/product/esp32/esp32C3SuperMini.html
+//
+//               USB
+//    5V                 GPIO5/MISO/A5
+//    GND                GPIO6/MOSI
+//    3V3                GPIO7/SS
+//    GPIO4/A4           GPIO8/SDA
+//    GPIO3/A3           GPIO9/SCL
+//    GPIO2/A2           GPIO10
+//    GPIO1/A1           GPIO22/RX
+//    GPIO0/A0           GPIO21/TX
+
+// Usage:
+//    GPIO4/A4        unused
+//    GPIO3/A3        Measure Control voltage
+//    GPIO2/A2        unused
+//    GPIO1/A1        Measure outter temperature sensor
+//    GPIO0/A0        Measure supply voltage
+//    GPIO5/MISO/A5   unused
+//    GPIO6/MOSI      unused
+//    GPIO7/SS        unused
+//    GPIO8/SDA       OLED
+//    GPIO9/SCL       OLED
+//    GPIO10          one-bit-D/A
+//    GPIO22/RX       unused
+//    GPIO21/TX       unused
+//
+// Measure Control Voltage and one-bit-D/A
+//    GND-1kOhm-A-10kOhm-B-4.3kOhm-Power
+//    B-240Ohm->Heizungkontrollspannung
+//    GND-EBC-102Ohm-B
+//    GND-100uF-B
+//    Basis-1kOhm-GPIO10
+//
+//
+// Measure Control Voltage at A
+//    GND-1kOhm-A-10kOhm-Power
+//
+// Measure outter temperature sensor
+//    GND-Rsensor1-4.3kOhm-3.3V
+//    Rsensor2-10kOhm-GPIO1
+//
 
 // can be used as parameter to tpl_command_setup
 // void execute(enum Command command) {}

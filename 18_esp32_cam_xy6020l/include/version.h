48f0145180ca935aa63e1de913920152b8666c8b
Jochen Kiemes <jochen@kiemes.de>
2026-02-23 14:27:16 +0100

    remove version.h from git

--- 18_esp32_cam_xy6020l/include/version.h
+++
@@ -1,8 +0,0 @@
-// Auto-generated placeholder
-#ifndef VERSION_H
-#define VERSION_H
-#define GIT_HASH "0000000"
-#define GIT_BRANCH "unknown"
-#define BUILD_TIME "1970-01-01 00:00:00"
-#define VERSION_STRING "unknown-0000000"
-#endif
